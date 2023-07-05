6eb625e2a0ac79eacbd6f576c52a85a3977bcc41
hftamayo <hftamayo@gmail.com>
2023-07-05 10:00:20 -0600

    data examples for demo purposes

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    name: "Herbert Tamayo",
+    email: "hftamayo@gmail.com",
+    id: 1,
+  },
+  {
+    name: "Sebastian Fernandez",
+    email: "sebastic@gmail.com",
+    id: 2,
+  },
+  {
+    name: "Guadalupe Martinez",
+    email: "gmartinez@gmail.com",
+    id: 3,
+  },
+];
