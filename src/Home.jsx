b2e48f6f5425e6d47580ed4f806ec924637b1e1f
hftamayo <hftamayo@gmail.com>
2023-07-05 10:37:33 -0600

    checking if the store is accesible from Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Home() {
-  return <div>Home</div>;
+  const users = useSelector((state) => state.users);
+  console.log(users);
+  return (
+    <div className="container">
+      <h2>Clients List</h2>
+      <button className="btn btn-success my-3">Create +</button>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Home;
