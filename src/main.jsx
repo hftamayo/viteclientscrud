b6d0f11700bc3992a07595be05ca850e9778a73d
hftamayo <hftamayo@gmail.com>
2023-07-05 10:37:13 -0600

    importing reducer into the main component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "./UserReducer.jsx";
 
 const store = configureStore({
   reducer: {
-    //user
+    users: UserReducer,
   },
 });
 
