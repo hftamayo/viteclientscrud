9a93f6e0fbf7fed2c316fc9b3c15f3f321d20df9
hftamayo <hftamayo@gmail.com>
2023-07-11 15:40:40 -0600

    coding create from part1

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Create() {
-  return <div>Create</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
+      <div className="w-50 border bg-secondary text-white p-5">
+        <h3>Add New Client</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Enter Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <br />
+          <button className="btn btn-info">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Create;
