79e6726755df32e9e49492d06741901934417d75
hftamayo <hftamayo@gmail.com>
2023-07-17 15:48:11 -0600

    update component part1

---
+++ src/Update.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Update() {
+  return (
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
+      <div className="w-50 border bg-secondary text-white p-5">
+        <h3>Update Client</h3>
+        <form>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Enter Name"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter Email"
+            />
+          </div>
+          <br />
+          <button className="btn btn-info">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Update;
