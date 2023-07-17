13038b8d2ffa10d0592d862aee9ecf74e1c5b165
hftamayo <hftamayo@gmail.com>
2023-07-17 15:37:46 -0600

    adding edit route

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/create" element={<Create />}></Route>
+      <Route path="/edit/:id" element={<Update />}></Route>      
     </Routes>
     
     </BrowserRouter>
