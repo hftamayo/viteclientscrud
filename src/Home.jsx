1bf1b285407fd25337957073aa5b1389ef4a5262
hftamayo <hftamayo@gmail.com>
2023-07-18 12:48:44 -0600

    delete method

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { deleteUser } from "./UserReducer";
 
 function Home() {
   const users = useSelector((state) => state.users);
-  console.log(users);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteUser({ id: id }));
+  };
+
   return (
     <div className="container">
       <h2>Clients List</h2>
@@ -33,7 +39,12 @@ function Home() {
                 >
                   Edit
                 </Link>
-                <button className="btn btn-sm btn-danger ms-2">Delete</button>
+                <button
+                  onClick={() => handleDelete(user.id)}
+                  className="btn btn-sm btn-danger ms-2"
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
