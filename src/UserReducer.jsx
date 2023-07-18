a81f44517f1ccbf8bb68a851c40f4c511c0b62a2
hftamayo <hftamayo@gmail.com>
2023-07-18 12:30:07 -0600

    adding deleteUser method

--- src/UserReducer.jsx
+++ src/UserReducer.jsx
@@ -16,8 +16,15 @@ const userSlice = createSlice({
         searchUser.email = email;
       }
     },
+    deleteUser: (state, action) => {
+      const { id } = action.payload;
+      const searchUser = state.find((user) => user.id == id);
+      if (searchUser) {
+        return state.filter((f) => f.id !== id);
+      }
+    },
   },
 });
 
-export const { addUser, updateUser } = userSlice.actions;
+export const { addUser, updateUser, deleteUser } = userSlice.actions;
 export default userSlice.reducer;
