3a7ee7c415fedc7ad52f9386ac5f698196bec020
hftamayo <hftamayo@gmail.com>
2023-07-18 11:44:23 -0600

    minor changes on update process

--- src/Update.jsx
+++ src/Update.jsx
@@ -3,18 +3,22 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 function Update() {
-  const {id} = useParams();
+  const { id } = useParams();
   const users = useSelector((state) => state.users);
-  const existingUser = users.filter(f => f.id == id);
-  const {name, email} = existingUser[0];
+  const existingUser = users.filter((f) => f.id == id);
+  const { name, email } = existingUser[0];
   const [uname, setName] = useState(name);
   const [uemail, setEmail] = useState(email);
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
       <div className="w-50 border bg-secondary text-white p-5">
         <h3>Update Client</h3>
-        <form>
+        <form onSubmit={handleUpdate}>
           <div>
             <label htmlFor="name">Name:</label>
             <input
@@ -23,6 +27,7 @@ function Update() {
               className="form-control"
               placeholder="Enter Name"
               value={uname}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div>
@@ -33,6 +38,7 @@ function Update() {
               className="form-control"
               placeholder="Enter Email"
               value={uemail}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <br />
